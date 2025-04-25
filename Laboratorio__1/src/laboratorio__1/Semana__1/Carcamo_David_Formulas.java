3057a1ec1c3da0a8e7a7f01c8550cb0323f94ab3
DJCC006 <david.carcamo2006@gmail.com>
2025-04-25 16:15:02 -0600

    Desarrollo de programa formulas y parte inicial

---
+++ Laboratorio__1/src/laboratorio__1/Semana__1/Carcamo_David_Formulas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio__1.Semana__1;
+
+/**
+ *
+ * @author David
+ */
+public class Carcamo_David_Formulas {
+    public static void main(String[] args){
+        Scanner IngresoNum1= new Scanner (System.in);
+        System.out.println("Ingrese el valor del numerador 1: ");
+        double Numerador1= IngresoNum1.nextDouble();
+        Scanner IngresoDen1= new Scanner (System.in);
+        System.out.println("Ingrese el valor del denominador 1: ");
+        double Denominador1= IngresoDen1.nextDouble();
+        
+        Scanner IngresoNum2= new Scanner (System.in);
+         System.out.println("Ingrese el valor del numerador 2: ");
+        double Numerador2= IngresoNum2.nextDouble();
+        Scanner IngresoDen2= new Scanner (System.in);
+        System.out.println("Ingrese el valor del denominador 2: ");
+        double Denominador2= IngresoDen2.nextDouble();
+    }
+}
