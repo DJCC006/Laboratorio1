b387fa67f9f3ab7f370ae8f1c0a616fa0f31a873
DJCC006 <david.carcamo2006@gmail.com>
2025-04-25 16:53:19 -0600

    Actualización 2 de ultimo programa

--- Laboratorio__1/src/laboratorio__1/Semana__1/Carcamo_David_Formulas.java
+++ Laboratorio__1/src/laboratorio__1/Semana__1/Carcamo_David_Formulas.java
@@ -3,14 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package laboratorio__1.Semana__1;
-
+import java.util.Scanner;
 /**
  *
  * @author David
  */
 public class Carcamo_David_Formulas {
     public static void main(String[] args){
-        Scanner IngresoNum1= new Scanner (System.in);
+       
+        
+        
+        
+        //Ej a
+         Scanner IngresoNum1= new Scanner (System.in);
         System.out.println("Ingrese el valor del numerador 1: ");
         double Numerador1= IngresoNum1.nextDouble();
         Scanner IngresoDen1= new Scanner (System.in);
@@ -23,5 +28,179 @@ public class Carcamo_David_Formulas {
         Scanner IngresoDen2= new Scanner (System.in);
         System.out.println("Ingrese el valor del denominador 2: ");
         double Denominador2= IngresoDen2.nextDouble();
+        double EjA=(Numerador1/Denominador1)+(Numerador2/Denominador2);
+        System.out.println("\n El resultado el ejercicio A es: " EjA);
+        
+        System.out.println("\n Resultado del A: "+EjA);
+        
+        
+        //Ej b
+        Scanner IngresoX= new Scanner (System.in);
+        System.out.println("Ingrese el valor de x: ");
+        double valorX= IngresoX.nextDouble();
+        Scanner IngresoY= new Scanner (System.in);
+        System.out.println("Ingrese el valor de y: ");
+        double valorY= IngresoY.nextDouble();
+        double EjB=(1/((valorX-5)-((3*valorX*valorY)/4)));
+        
+        System.out.println("\n Resultado del b: "+EjB);
+          
+        //Ej c
+        double EjC= ((1/2)+7);
+        
+        System.out.println("\n Resultado del c: "+EjC);
+        
+        //Ej d
+        double EjD= (7+(1/2));
+        
+        
+        System.out.println("\n Resultado del d: "+EjD);
+        
+        //Ej e
+        Scanner IngresoA2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de a: ");
+        double valorA2= IngresoA2.nextDouble();
+        Scanner IngresoB= new Scanner (System.in);
+        System.out.println("Ingrese el valor de b: ");
+        double valorB= IngresoB.nextDouble();
+        Scanner IngresoC= new Scanner (System.in);
+        System.out.println("Ingrese el valor de c: ");
+        double valorC= IngresoC.nextDouble();
+        Scanner IngresoD= new Scanner (System.in);
+        System.out.println("Ingrese el valor de d: ");
+        double valorD= IngresoD.nextDouble();
+        Scanner IngresoE= new Scanner (System.in);
+        System.out.println("Ingrese el valor de e: ");
+        double valorE= IngresoE.nextDouble();
+        Scanner IngresoF= new Scanner (System.in);
+        System.out.println("Ingrese el valor de f: ");
+        double valorF= IngresoF.nextDouble();
+        Scanner IngresoG= new Scanner (System.in);
+        System.out.println("Ingrese el valor de g: ");
+        double valorG= IngresoG.nextDouble();
+        Scanner IngresoH= new Scanner (System.in);
+        System.out.println("Ingrese el valor de H: ");
+        double valorH= IngresoH.nextDouble();
+        Scanner IngresoJ= new Scanner (System.in);
+        System.out.println("Ingrese el valor de j: ");
+        double valorJ= IngresoJ.nextDouble();
+        
+        double EjE= (((valorA2*valorA2)/(valorB-valorC))+((valorD-valorE)/(valorF-((valorG*valorH)/valorJ))));
+        
+        System.out.println("\n Resultado del e: "+EjE);
+        
+        
+        //Ej f
+        Scanner IngresoM= new Scanner (System.in);
+        System.out.println("Ingrese el valor de m: ");
+        double valorM= IngresoM.nextDouble();
+        Scanner IngresoN= new Scanner (System.in);
+        System.out.println("Ingrese el valor de n: ");
+        double valorN= IngresoN.nextDouble();
+        Scanner IngresoP= new Scanner (System.in);
+        System.out.println("Ingrese el valor de p: ");
+        double valorP= IngresoP.nextDouble();
+        
+        double EjP= ((valorM/valorN)+(valorP));
+        
+        System.out.println("\n Resultado del f: "+EjF);
+        
+        //Ej g
+        Scanner IngresoM2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de m: ");
+        double valorM2= IngresoM2.nextDouble();
+        Scanner IngresoN2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de n: ");
+        double valorN2= IngresoN2.nextDouble();
+        Scanner IngresoP2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de p: ");
+        double valorP2= IngresoP2.nextDouble();
+        Scanner IngresoQ= new Scanner (System.in);
+        System.out.println("Ingrese el valor de q: ");
+        double valorQ= IngresoQ.nextDouble();
+        
+        double EjQ= (valorM2+(valorN2/(valorP2-valorQ)));
+        
+        System.out.println("\n Resultado del g: "+EjG);
+        
+        //Ej h
+        Scanner IngresoA3= new Scanner (System.in);
+        System.out.println("Ingrese el valor de a: ");
+        double valorA3= IngresoA3.nextDouble();
+        Scanner IngresoB2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de b: ");
+        double valorB2= IngresoB2.nextDouble();
+        Scanner IngresoC2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de c: ");
+        double valorC2= IngresoC2.nextDouble();
+        Scanner IngresoD2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de d: ");
+        double valorD2= IngresoD2.nextDouble();
+        
+        double EjH= (((valorA3*valorA3)/(valorB2*valorB2))+((valorC2*valorC2)/(valorD2*valorD2)));
+        
+        System.out.println("\n Resultado del h: "+EjH);
+        
+        //Ej i
+        Scanner IngresoM3= new Scanner (System.in);
+        System.out.println("Ingrese el valor de m: ");
+        double valorM3= IngresoM3.nextDouble();
+        Scanner IngresoN3= new Scanner (System.in);
+        System.out.println("Ingrese el valor de n: ");
+        double valorN3= IngresoN3.nextDouble();
+        Scanner IngresoP3= new Scanner (System.in);
+        System.out.println("Ingrese el valor de p: ");
+        double valorP3= IngresoP3.nextDouble();
+        Scanner IngresoQ2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de q: ");
+        double valorQ2= IngresoQ2.nextDouble();
+        
+       
+        Scanner IngresoR= new Scanner (System.in);
+        System.out.println("Ingrese el valor de r: ");
+        double valorR= IngresoR.nextDouble();
+        Scanner IngresoS= new Scanner (System.in);
+        System.out.println("Ingrese el valor de s: ");
+        double valorS= IngresoS.nextDouble();
+        
+        double EjI= ((valorM3+(valorN3/valorP3))/(valorQ2-(valorR/valorS)));
+        
+        
+        System.out.println("\n Resultado del i: "+EjI);
+        
+        //Ej j
+        Scanner IngresoA4= new Scanner (System.in);
+        System.out.println("Ingrese el valor de a: ");
+        double valorA4= IngresoA4.nextDouble();
+        Scanner IngresoB3= new Scanner (System.in);
+        System.out.println("Ingrese el valor de b: ");
+        double valorB3= IngresoB3.nextDouble();
+        Scanner IngresoC3= new Scanner (System.in);
+        System.out.println("Ingrese el valor de c: ");
+        double valorC3= IngresoC3.nextDouble();
+        Scanner IngresoD3= new Scanner (System.in);
+        System.out.println("Ingrese el valor de d: ");
+        double valorD3= IngresoD3.nextDouble();
+        
+        Scanner IngresoE2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de e: ");
+        double valorE2= IngresoE2.nextDouble();
+        
+        Scanner IngresoF2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de f: ");
+         double valorF2= IngresoF2.nextDouble();
+        Scanner IngresoG2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de g: ");
+        double valorG2= IngresoG2.nextDouble();
+        Scanner IngresoH2= new Scanner (System.in);
+        System.out.println("Ingrese el valor de H: ");
+        double valorH2= IngresoH2.nextDouble();
+        
+        double EjJ= (((3*valorA4)+valorB3)/(valorC3-((valorD3+(5*valorE2))/(valorF2+(valorG2/(2*valorH2))))));
+        
+        System.out.println("\n Resultado del J: "+EjJ);
+        
+      
+ 
     }
 }
