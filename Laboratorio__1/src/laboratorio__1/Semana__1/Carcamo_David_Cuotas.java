3a21e4c35913443fe32040776fafc0da15deff69
DJCC006 <david.carcamo2006@gmail.com>
2025-04-25 15:15:52 -0600

    Creación y desarrollo de ejercicio de cuotas
    Se creó el proyecto de cuotas y se desarrolla la parte inicial del programa

---
+++ Laboratorio__1/src/laboratorio__1/Semana__1/Carcamo_David_Cuotas.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio__1.Semana__1;
+import java.util.Scanner;
+/**
+ *
+ * @author David
+ */
+public class Carcamo_David_Cuotas {
+    public static void main(String[] args){
+        //Ingreso de datos referente al prestamo del cliente, los cuales influirán en la cuota
+        Scanner IngresoPresta= new Scanner(System.in);
+        System.out.println("Ingrese el valor del prestamo otorgado al cliente: ");
+        double Prestamo = IngresoPresta.nextDouble();
+        
+        Scanner IngresoInteres = new Scanner(System.in);
+        System.out.println("\n Ingrese el porcentaje de interes que se le aplica al préstamo (En forma no decimal): ");
+        int InteresEntero = IngresoInteres.nextInt();
+        double InteresDecimal= InteresEntero/100;
+        
+        Scanner IngresoComi= new Scanner(System.in);
+        System.out.println("\n Ingrese el valor de la comisión por cuota aplicada al préstamo: ");
+        double Comision= IngresoComi.nextDouble();
+        
+        Scanner IngresoSeguro = new Scanner(System.in);
+        System.out.println("\n Ingrese el porcentaje de seguro aplicado a la cuota (En forma no decimal): ");
+        int SeguroEntero= IngresoSeguro.nextInt();
+        double Seguro = SeguroEntero/100;
+        
+        Scanner IngresoPlazo = new Scanner (System.in);
+        System.out.println("\n Ingrese el plazo establecido para pagar el préstamo (en meses): ");
+        int Plazo= IngresoPlazo.nextInt();
+        
+        
+        
+        
+    }
+}
