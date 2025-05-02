bbcaeaae9550c289ddeb547fdb07e455c7511a90
DJCC006 <david.carcamo2006@gmail.com>
2025-05-02 14:36:33 -0600

    Creacion proyecto de tiempo

---
+++ Laboratorio__1/src/laboratorio__1/Semana_2/Carcamo_David_Tiempo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio__1.Semana_2;
+
+/**
+ *
+ * @author David
+ */
+public class Carcamo_David_Tiempo {
+    public static void main(String[] args){
+        
+    }
+}
