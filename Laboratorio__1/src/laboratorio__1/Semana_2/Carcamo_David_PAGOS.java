f722b438a316213dcd53bf2ae817231c9f4ae410
DJCC006 <david.carcamo2006@gmail.com>
2025-05-02 15:53:32 -0600

    desarrollo de estructura de ciclos con switch

--- Laboratorio__1/src/laboratorio__1/Semana_2/Carcamo_David_PAGOS.java
+++ Laboratorio__1/src/laboratorio__1/Semana_2/Carcamo_David_PAGOS.java
@@ -11,8 +11,79 @@ import java.util.Scanner;
 public class Carcamo_David_PAGOS {
     public static void main(String[] args){
         
+        //Ingreso de Datos personales
+        
+        Scanner lea= new Scanner(System.in);
+        lea.useDelimiter("\n");
+        
+        System.out.println("Ingrese el codigo del empleado: ");
+        int codigoEmpleado=lea.nextInt();
+        System.out.println("Ingrese el primer nombre del empleado: ");
+        String firstName= lea.next();
+        System.out.println("Ingrese el primer apellido del empleado: ");
+        String firstLastName= lea.next();
+        System.out.println("Ingrese el total de horas trabajadas: ");
+        int cantidadhoras=lea.nextInt();
+        System.out.println("Ingrese la categoria correspondiente: ");
+        int categoria= lea.nextInt();
+        
+        
+       
+        //Evaluacion con switch y ciclos ifs
+        int horasExtra=0;
+        double pagoExtra=0;
+        double subtotal=0;
+        if(cantidadhoras>40){
+            horasExtra=cantidadhoras-40;
+            if(horasExtra>15){
+                switch(categoria){
+                    case 1:
+                        pagoExtra=40*15;
+                        subtotal=35.99*40;
+                        break;
+                    case 2:
+                        pagoExtra=50*15;
+                        subtotal=35.99*40;
+                        break;
+                    case 3:
+                        pagoExtra=85*15;
+                        subtotal=35.99*40;
+                        break;
+                    case 4:
+                        pagoExtra=0;
+                        subtotal=35.99*40;
+                        break;
+
+                }
+            }else{
+                switch(categoria){
+                    case 1:
+                        pagoExtra=40*horasExtra;
+                        subtotal=35.99*40;
+                        break;
+                    case 2:
+                        pagoExtra=50*horasExtra;
+                        subtotal=35.99*40;
+                        break;
+                    case 3:
+                        pagoExtra=85*horasExtra;
+                        subtotal=35.99*40;
+                        break;
+                    case 4:
+                        pagoExtra=0;
+                        subtotal=35.99*40;
+                        break;
+            }
+
+            
+        }
+        
+    }else{
+            subtotal=35.99*cantidadhoras;
+        }
+        
+        
+      
     }
-    
-    
-    
+        
 }
