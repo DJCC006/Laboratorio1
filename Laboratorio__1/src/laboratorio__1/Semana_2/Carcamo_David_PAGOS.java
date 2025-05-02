87c838cc75a436029ab8c8752243a7ba2a80cfc2
DJCC006 <david.carcamo2006@gmail.com>
2025-05-02 15:27:02 -0600

    Creacion de proyecto de pagos

---
+++ Laboratorio__1/src/laboratorio__1/Semana_2/Carcamo_David_PAGOS.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio__1.Semana_2;
+import java.util.Scanner;
+/**
+ *
+ * @author David
+ */
+public class Carcamo_David_PAGOS {
+    public static void main(String[] args){
+        
+    }
+    
+    
+    
+}
