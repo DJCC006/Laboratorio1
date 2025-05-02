1358a21f3c65c657b303af4e26913407be4cf133
DJCC006 <david.carcamo2006@gmail.com>
2025-05-02 16:10:51 -0600

    Lectura inicial

---
+++ Laboratorio__1/src/laboratorio__1/Semana_2/Carcamo_David_Fecha.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio__1.Semana_2;
+import java.util.Scanner;
+/**
+ *
+ * @author David
+ */
+public class Carcamo_David_Fecha {
+    public static void main(String[] args){
+        Scanner lea= new Scanner(System.in);
+        lea.useDelimiter("\n");
+        //Lectura de fechas
+        System.out.println("Porfavor ingrese la primer fecha en el formato Dia/Mes/Año: ");
+        String fechaone=lea.next();
+        System.out.println("Porfavor ingrese la segunda fecha en el formato Dia/Mes/Año: ");
+        String fechatwo=lea.next();
+        
+        
+    }
+}
