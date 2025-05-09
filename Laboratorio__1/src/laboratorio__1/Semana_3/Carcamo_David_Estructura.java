f403b6d45d31647d7adb0314b57d7ad83dd32e67
DJCC006 <david.carcamo2006@gmail.com>
2025-05-09 13:13:16 -0600

    Creacion de trabajo laboratorio semana 3

---
+++ Laboratorio__1/src/laboratorio__1/Semana_3/Carcamo_David_Estructura.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio__1.Semana_3;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author David
+ */
+public class Carcamo_David_Estructura {
+    
+    public static void main(String[] args){
+        
+        boolean status=true;
+        
+        while(status==true){
+            
+            Scanner lea= new Scanner(System.in);
+            lea.useDelimiter("\n");
+            
+            
+            
+            
+            
+        }
+        
+        
+    }
+    
+    
+}
