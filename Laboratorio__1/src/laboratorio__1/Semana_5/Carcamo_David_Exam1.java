7bcf4ff6993f83488001661d0c3dfdad1f9c89c2
DJCC006 <david.carcamo2006@gmail.com>
2025-05-23 13:11:48 -0600

    Creacion de estructura general

--- Laboratorio__1/src/laboratorio__1/Semana_5/Carcamo_David_Exam1.java
+++ Laboratorio__1/src/laboratorio__1/Semana_5/Carcamo_David_Exam1.java
@@ -4,10 +4,54 @@
  */
 package laboratorio__1.Semana_5;
 
+import java.util.Scanner;
+
 /**
  *
  * @author David
  */
 public class Carcamo_David_Exam1 {
-    
+    public static void main(String[] args) {
+        Scanner lea= new Scanner(System.in);
+        lea.useDelimiter("\n");
+        
+        boolean statusPrograma=true;
+        
+        while(statusPrograma==true){
+            System.out.println("Porfavor seleccione una opción: ");
+            int opcion=lea.nextInt();
+            
+            switch(opcion){
+                
+                case 1:
+                    
+                    
+                    break;
+                
+                case 2:
+                    
+                    break;
+                
+                case 3:
+                    
+                    break;
+                    
+                case 4:
+                    
+                    break;
+                    
+                case 5:
+                    
+                    break;
+                    
+                default:
+                            
+                    
+            }//Fin de switch
+            
+       
+            
+        }
+        
+    }
 }
