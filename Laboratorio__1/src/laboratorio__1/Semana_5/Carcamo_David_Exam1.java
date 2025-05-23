53d495e1f151a96a34657aa4f57d310d5b00b89f
DJCC006 <david.carcamo2006@gmail.com>
2025-05-23 15:08:04 -0600

    update

--- Laboratorio__1/src/laboratorio__1/Semana_5/Carcamo_David_Exam1.java
+++ Laboratorio__1/src/laboratorio__1/Semana_5/Carcamo_David_Exam1.java
@@ -66,7 +66,109 @@ public class Carcamo_David_Exam1 {
                 
                 case 2:
                     //ejercicio clave
-                    System.out.println("Entra a case 2");
+                    System.out.println("");
+                    System.out.println("Elija una opcion: ");
+                    System.out.println("1.Cifrar"+"\n2.Descrifar");
+                    int opcion2=lea.nextInt();
+                    
+                    String group1="abcdefghijklm";
+                    String group2="zyxwvutsrqpon";
+                    String abecedario="abcdefghijklmnopqrstuvwxyz";
+                    
+                    
+                    switch(opcion2){
+                        case 1:
+                            System.out.println("");
+                            System.out.println("Ingrese mensaje a crifrar: ");
+                            String mensaje=lea.next();
+                            
+                            int length=mensaje.length();
+                            
+                            int k;
+                            int lengthabc=abecedario.length();
+                            for(k=0; k<=(length-1);k++){
+                                System.out.println("entra aqui");
+                                char letra= mensaje.charAt(k);
+                                
+                                int l;
+                                char letraopuesta=' ';
+                                
+                                //Ubicacion abecedario general
+                                for(l=0;l<=(lengthabc-1);l++){
+                                    char letraprovisional=abecedario.charAt(l);
+                                    
+                                    int pos=0; 
+                                    if(letraprovisional==letra){
+                                            pos= abecedario.indexOf(l);
+                                            int pos1=0;
+                                            int pos2=0;
+
+                                            //Ubicacion por subgrupos
+                                            if(pos>=0 && pos<=13){
+                                                int o;
+
+                                                //Ubicarse en su grupo incial
+                                                int lengthgroup1= group1.length();
+                                                for(o=0; o<=(lengthgroup1-1);o++){
+                                                    char letrag1=group1.charAt(o);
+                                                    if(letrag1==letra){
+                                                        pos1=group1.indexOf(o);
+                                                    }
+
+                                                }
+
+                                                //ubicarse en su grupo alterno
+                                                int lengthgroup2=group2.length();
+                                                for(o=0; o<=(lengthgroup2-1);o++){
+                                                    int pos3=group2.indexOf(o);
+                                                    if(pos3==pos1){
+                                                        letraopuesta=group2.charAt(o);
+                                                        System.out.println(letraopuesta);
+                                                    }   
+
+                                                }
+                                            }else if(pos>=14 && pos<=26){
+                                                int o;
+                                                int lengthgroup2= group2.length();
+                                                for(o=0; o<(lengthgroup2-1);o++){
+                                                    char letrag2=group2.charAt(o);
+                                                    if(letrag2==letra){
+                                                        pos1=group2.indexOf(o);
+                                                    }
+
+
+                                                //ubicarse en su grupo alterno
+                                                int lengthgroup1=group1.length();
+                                                for(o=0; o<=(lengthgroup1-1);o++){
+                                                    int pos3=group1.indexOf(o);
+                                                    if(pos3==pos1){
+                                                        letraopuesta=group1.charAt(o);
+                                                        System.out.println(letraopuesta);
+                                                    }
+
+                                                    }
+
+                                                }
+
+
+
+                                                }
+                                
+                                    }
+                                }
+                                
+                                
+                                
+                                System.out.println(letraopuesta);
+                            
+                   }
+                            break;
+                            
+                case 2:
+                            break;
+                            
+                        default:
+                    }
                     
                     
                     break;
@@ -185,3 +287,4 @@ public class Carcamo_David_Exam1 {
         
     }
 }
+
