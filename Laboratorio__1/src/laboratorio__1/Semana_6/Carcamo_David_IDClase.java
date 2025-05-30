43b1c0f380c0bbf55f62a7f2c5ef7ae315f63a81
DJCC006 <david.carcamo2006@gmail.com>
2025-05-30 14:26:09 -0600

    CREACION DE trabajo arreglos

---
+++ Laboratorio__1/src/laboratorio__1/Semana_6/Carcamo_David_IDClase.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio__1.Semana_6;
+
+import java.util.Random;
+import java.util.Scanner;
+/**
+ *
+ * @author David
+ */
+public class Carcamo_David_IDClase extends javax.swing.JFrame {
+    
+    public static void main(String[] args) {
+        //Parte donde se crea el arreglo inicial
+        String palabras[]={"casa","gato","perro","camisa","zapato","aeroplano","paracetamol","onomatopeya","ciudad"};
+
+        //Cantidad de oportunidad
+        int oportunidades= 5;
+
+
+        new PantalladeJuego().setVisible(true);
+    }
+    
+
+}
